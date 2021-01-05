4d8edc8148cb96c37145f9ce47e4725be6262a6d
Artem Mikhailov <mega.artemm@bk.ru>
2021-01-05 16:55:13 +0300

    error

--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/MainActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/MainActivity.java
@@ -1,13 +1,25 @@
 package ru.aomikhailov.chessdrawmastercdm;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public  class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ButtonStartScreen = (Button) findViewById(R.id.ButtonWithOutLogin);
+        ButtonStartScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StartScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/StartScreenActivity.java
@@ -0,0 +1,13 @@
+package ru.aomikhailov.chessdrawmastercdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
