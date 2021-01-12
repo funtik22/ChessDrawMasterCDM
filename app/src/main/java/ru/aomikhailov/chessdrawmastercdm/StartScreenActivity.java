3fa71ce007c3141cde9a90d7533889ea7136e86f
Artem Mikhailov <mega.artemm@bk.ru>
2021-01-12 15:23:56 +0300

    Very good

--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/StartScreenActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/StartScreenActivity.java
@@ -8,6 +8,6 @@ public class StartScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.startscreen);
     }
 }
