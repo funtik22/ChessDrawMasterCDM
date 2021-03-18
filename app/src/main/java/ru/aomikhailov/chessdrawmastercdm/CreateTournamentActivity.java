bbdf22f8502733cd6ed01aa281ffe52139dcbc94
Artem Mikhailov <mega.artemm@bk.ru>
2021-03-18 14:44:38 +0300

    new design

---
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/CreateTournamentActivity.java
@@ -0,0 +1,14 @@
+package ru.aomikhailov.chessdrawmastercdm;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateTournamentActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_tournament);
+    }
+}
--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/StartScreenActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/StartScreenActivity.java
@@ -1,14 +1,28 @@
 package ru.aomikhailov.chessdrawmastercdm;
 
+import android.content.Intent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StartScreenActivity extends AppCompatActivity {
 
+
+    ImageButton CreateTournament;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen);
+        CreateTournament = findViewById(R.id.ButtonCreateTournament);
+        CreateTournament.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartScreenActivity.this, CreateTournamentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
