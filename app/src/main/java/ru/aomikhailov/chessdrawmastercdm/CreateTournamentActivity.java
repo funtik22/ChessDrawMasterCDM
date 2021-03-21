ab282762765a797e477f3f1476cd3e5fa9e87403
Artem Mikhailov <mega.artemm@bk.ru>
2021-03-21 10:20:58 +0300

    Menu

--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/CreateTournamentActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/CreateTournamentActivity.java
@@ -1,14 +1,68 @@
 package ru.aomikhailov.chessdrawmastercdm;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateTournamentActivity extends AppCompatActivity {
 
+    ImageButton MenuButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_tournament);
+
+        MenuButton = findViewById(R.id.MenuButton);
+        MenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
     }
-}
+            private void showPopupMenu(View v) {
+                PopupMenu popupMenu = new PopupMenu(this, v);
+                popupMenu.inflate(R.menu.populmenu);
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(MenuItem item) {
+                                switch (item.getItemId()) {
+                                    case R.id.menu1:
+                                        Toast.makeText(getApplicationContext(),
+                                                "Вы выбрали PopupMenu 1",
+                                                Toast.LENGTH_SHORT).show();
+                                        return true;
+                                    case R.id.menu2:
+                                        Toast.makeText(getApplicationContext(),
+                                                "Вы выбрали PopupMenu 2",
+                                                Toast.LENGTH_SHORT).show();
+                                        return true;
+                                    case R.id.menu3:
+                                        Toast.makeText(getApplicationContext(),
+                                                "Вы выбрали PopupMenu 3",
+                                                Toast.LENGTH_SHORT).show();
+                                        return true;
+                                    default:
+                                        return false;
+                                }
+                            }
+                        });
+
+                popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+                    @Override
+                    public void onDismiss(PopupMenu menu) {
+                        Toast.makeText(getApplicationContext(), "onDismiss",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                popupMenu.show();
+            }
+        }
+
+
+
--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/RegisterActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/RegisterActivity.java
@@ -1,12 +1,26 @@
 package ru.aomikhailov.chessdrawmastercdm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ImageButton ButtonZaRegister;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        ButtonZaRegister = findViewById(R.id.ButtonZaRegister);
+        ButtonZaRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
