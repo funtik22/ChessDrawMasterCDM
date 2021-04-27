b959ac9ebc84f240e4b2945437d9ca4fad4aacee
Artem Mikhailov <mega.artemm@bk.ru>
2021-04-27 18:02:53 +0300

    TableActivity+TournamentActivity

--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/TournamentActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/TournamentActivity.java
@@ -1,6 +1,9 @@
 package ru.aomikhailov.chessdrawmastercdm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,15 +12,22 @@ import java.util.List;
 
 public class TournamentActivity extends AppCompatActivity {
 
-    TextView textView;
+Button OpenTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tournament);
         List <Player> playerList = (List<Player>) getIntent().getSerializableExtra(Constants.PLAYER_LIST_NAME);
-        textView = findViewById(R.id.textView9);
-        textView.setText(playerList.get(0).getName().toString());
+        OpenTable = findViewById(R.id.ButtonOpenTable);
+        OpenTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TournamentActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
