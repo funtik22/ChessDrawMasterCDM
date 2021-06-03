4321a01804b9f3f041918e13b9e568bf50435455
Artem Mikhailov <mega.artemm@bk.ru>
2021-06-03 11:35:24 +0300

    Authentication

--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/MainActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/MainActivity.java
@@ -2,20 +2,37 @@ package ru.aomikhailov.chessdrawmastercdm;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity  {
 
    Button ButtonContinueWithoutLogin ;
    Button ButtonRegister;
    Button ButtonLogin;
+   TextInputEditText edEmail;
+    TextInputEditText edPassword;
+   private FirebaseAuth mAuth;
+   private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
         ButtonContinueWithoutLogin = findViewById(R.id.buttonContinueWithoutLogin);
         ButtonRegister = findViewById(R.id.buttonRegister);
         ButtonLogin = findViewById(R.id.buttonLogin);
+        mAuth = FirebaseAuth.getInstance();
+        edEmail = findViewById(R.id.email1);
+        edPassword = findViewById(R.id.password1);
+
 
         ButtonContinueWithoutLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +66,25 @@ public class MainActivity extends AppCompatActivity {
         ButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), R.string.InWork, Toast.LENGTH_SHORT).show();
+                signing(edEmail.getText().toString(), edPassword.getText().toString());
             }
         });
     }
+    public void signing(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Intent intent = new Intent(MainActivity.this, StartScreenActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(MainActivity.this, R.string.ZaRg, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
 }
+
--- app/src/main/java/ru/aomikhailov/chessdrawmastercdm/RegisterActivity.java
+++ app/src/main/java/ru/aomikhailov/chessdrawmastercdm/RegisterActivity.java
@@ -4,23 +4,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Button ButtonRegister;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    TextInputEditText edMail;
+    TextInputEditText edPassword;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
         ButtonRegister = findViewById(R.id.buttonRegisterNow);
-
+        edMail = findViewById(R.id.email);
+        edPassword = findViewById(R.id.password);
         ButtonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
+                registration(edMail.getText().toString(), edPassword.getText().toString());
+
+            }
+        });
+    }
+
+    public void registration (String email,String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(RegisterActivity.this, R.string.RgSus, Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(RegisterActivity.this, R.string.Warnings, Toast.LENGTH_LONG).show();
             }
         });
     }
